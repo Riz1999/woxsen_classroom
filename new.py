c52aa5049c792d1bb21f5a43d6a677080ce6f399
rizwan zhad <92221232+Riz1999@users.noreply.github.com>
2023-12-21 16:41:54 +0530

    Update new.py

--- new.py
+++ new.py
@@ -119,6 +119,15 @@ def send_email_notification(name, mail_id, sport_type, slot_time,sport_type1,slo
 
 
 def slot_main():
+    disclaimer_html = """
+        <div style="background-color: #f0f0f0; padding: 10px; border-radius: 5px; margin-top: 20px;">
+            <p><strong>Disclaimer:</strong></p>
+            <p>1. Each user can book a maximum of 2 sessions per day.</p>
+            <p>2. Please book classes according to the number of students expected to attend the session.</p>
+        </div>
+    """
+
+    st.markdown(disclaimer_html, unsafe_allow_html=True)
     col1, col2, col3 = st.columns([0.4, 1, 0.2])
     with col2:
         st.image("Logo.png", width=300)
